24c522968a05c35f59f24dfd2959e21beb252d17
Demid <lliker1337@gmail.com>
2024-02-14 18:11:00 +0300

    2

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from gendiff.cli import parse
+
+
+def main():
+    args = parse()
+    print(
+        generate_diff(args.first_file, args.second_file, formater=args.format))
+
+
+if __name__ == '__main__':
+    main()
