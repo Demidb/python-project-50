0ec84752131564698e90bdf1de42303168d96e2a
Demidb <LLiker1337@gmail.com>
2024-05-20 18:11:55 +0300

    5

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python3
 import os
 
 
-def get_extension(file_path):
-    extension = os.path.splitext(file_path)[1]
-    return extension[1:]
+def get_extension(file_name):
+    current_dir = os.path.dirname(__file__)
+    return os.path.join(current_dir, 'fixtures', file_name)
+
+
+def get_data(expected_result):
+    with open(get_fixtures_path(expected_result), "r") as correct:
+        return correct.read()
