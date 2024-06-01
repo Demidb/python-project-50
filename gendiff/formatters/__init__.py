a342ef30bb7455a0b8e51adc448a61a2359ee9c7
Demidb <LLiker1337@gmail.com>
2024-06-01 12:20:23 +0300

    5.9

---
+++ gendiff/formatters/__init__.py
@@ -0,0 +1,18 @@
+from .stylish_format import stylish
+from .plain_format import plain
+from .json_format import json_form
+
+
+def format(diff_tree, formatter):
+    match formatter:
+        case 'stylish':
+            print(stylish(diff_tree))
+            return stylish(diff_tree)
+        case 'plain':
+            print(plain(diff_tree))
+            return plain(diff_tree)
+        case 'json':
+            print(json_form(diff_tree))
+            return json_form(diff_tree)
+        case _:
+            ValueError(f'Unsupported format: {formatter}')
