7efebb417760777db7c67f5c93319a5e9b1f6d58
Demidb <LLiker1337@gmail.com>
2024-06-17 14:20:37 +0300

    5.9.3

--- gendiff/scripts/generate_differance.py
+++ gendiff/scripts/generate_differance.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
 from gendiff.cli import parser
+from gendiff.generate_difference import generate_diff
 
 
 def main():
-    parser()
+    args = parser()
+    generate_diff(args.first_file, args.second_file, args.format)
 
 
 if __name__ == '__main__':
