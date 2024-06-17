7726dc3198f9ff6ddd01e16b550cf8ecd27ddd6b
Demidb <LLiker1337@gmail.com>
2024-06-17 14:44:16 +0300

    5

--- gendiff/formatters/plain_format.py
+++ gendiff/formatters/plain_format.py
@@ -1,6 +1,5 @@
 from gendiff.consts import TYPES
 
-
 def checking_value(value):
     if isinstance(value, (bool, int)):
         return str(value).lower()
@@ -8,9 +7,7 @@ def checking_value(value):
         return 'null'
     if isinstance(value, dict):
         return '[complex value]'
-
     return f"'{value}'"
-
 
 def plain(data):
     def _iter_plain(data, path=''):
@@ -21,19 +18,15 @@ def plain(data):
                 case TYPES.DELETED:
                     result.append(f"Property '{print_path}' was removed")
                 case TYPES.ADDED:
-                    result.append(f"Property '{print_path}' was added with "
-                                  f"value: {checking_value(value['value'])}")
+                    result.append(f"Property '{print_path}' was added with value: {checking_value(value['value'])}")
                 case TYPES.CHANGED:
-                    result.append(f"Property '{print_path}' was updated. "
-                                  f"From {checking_value(value['old_value'])} "
-                                  f"to {checking_value(value['new_value'])}")
+                    result.append(f"Property '{print_path}' was updated. From {checking_value(value['old_value'])} to {checking_value(value['new_value'])}")
                 case TYPES.NESTED:
                     result.extend(_iter_plain(value['value'], print_path))
                 case TYPES.UNCHANGED:
                     continue
                 case _:
                     raise ValueError(f'Unknown type: {value["type"]}')
-
         return result
 
     result_string = '\n'.join(_iter_plain(data))
